58fbb832ebd45131dc4be9ac1eb3f11d3216177e
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-05-06 04:27:29 +0800

    Frist commit to Githb add MainActivity add HomePageActivity

---
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/HomePageActivity.kt
@@ -0,0 +1,12 @@
+package com.jackpan.specialstudy.oveyouforyourtravel
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}

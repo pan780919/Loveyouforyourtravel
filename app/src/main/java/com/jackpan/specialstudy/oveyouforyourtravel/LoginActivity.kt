149ee6236734bd1d9c85c88265d061312b0a22af
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-05-06 04:39:06 +0800

    add LoginActivity and remove some code

---
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.jackpan.specialstudy.oveyouforyourtravel
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity() {
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // Set up the login form
+    }
+
+
+}

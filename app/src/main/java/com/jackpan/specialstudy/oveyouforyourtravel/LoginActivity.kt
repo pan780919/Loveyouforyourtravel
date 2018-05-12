20c482f640574aa538489470a8512d89ed64543c
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-05-12 22:45:27 +0800

    add singupActivity

--- app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/LoginActivity.kt
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.jackpan.specialstudy.oveyouforyourtravel
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 /**
  * A login screen that offers login via email/password.
@@ -10,11 +12,33 @@ class LoginActivity : AppCompatActivity() {
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
+    lateinit var  mLoginBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mLoginBtn = findViewById(R.id.loginbtn)
+
         // Set up the login form
     }
+    fun login(emaial :String,password:String){
+        if(emaial.equals("")){
+            showDilog("請輸入帳號！")
+            return
+        }
+        if (password.equals("請輸入密碼")){
+            showDilog("")
 
+            return
+        }
+
+    }
+
+    fun showDilog(message:String){
+        var mAlertDilog = AlertDialog.Builder(this)
+        mAlertDilog.setTitle("提示")
+        mAlertDilog.setMessage(message)
+        mAlertDilog.setPositiveButton("知道了!",null)
+        mAlertDilog.show()
+    }
 
 }
---
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.jackpan.specialstudy.oveyouforyourtravel
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+}

d9443601852eab3a3489b609c244b11bbf1ee5a0
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-05-19 16:59:18 +0800

    add view for sigupActivity

--- app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/SignUpActivity.kt
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/SignUpActivity.kt
@@ -2,19 +2,42 @@ package com.jackpan.specialstudy.oveyouforyourtravel
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import com.jackpan.libs.mfirebaselib.MfiebaselibsClass
 import com.jackpan.libs.mfirebaselib.MfirebaeCallback
 
 class SignUpActivity : AppCompatActivity(), MfirebaeCallback {
-
+    lateinit var  mNaamEdt :EditText
+    lateinit var mDatadedt :EditText
+    lateinit var mPhoneedt :EditText
+    lateinit var mEmailedt :EditText
+    lateinit var mPasswordedt :EditText
+    lateinit var mPasswordedt_2 :EditText
+    lateinit var mSignupbtn :Button
 
     lateinit var mFirebselibClass : MfiebaselibsClass
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         mFirebselibClass =  MfiebaselibsClass(this,this)
+        mNaamEdt = findViewById(R.id.nameedt)
+        mDatadedt =findViewById(R.id.datadedt)
+        mPhoneedt = findViewById(R.id.phoneedt)
+        mEmailedt = findViewById(R.id.emaidedt)
+        mPasswordedt = findViewById(R.id.passwordedt)
+        mPasswordedt_2 = findViewById(R.id.passwordedt_2)
+        mSignupbtn = findViewById(R.id.signupbtn)
+
+        mSignupbtn.setOnClickListener {
+
+            sigup()
+        }
 
 
+
+    }
+    fun sigup(){
 
     }
     override fun resetPassWordState(p0: Boolean) {

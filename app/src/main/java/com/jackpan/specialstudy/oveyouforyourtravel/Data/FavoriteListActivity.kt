f2e62808345746a2151bcadb9850102256994975
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-06-24 17:09:49 +0800

    add  FavoriteListActivity Class

---
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/Data/FavoriteListActivity.kt
@@ -0,0 +1,13 @@
+package com.jackpan.specialstudy.oveyouforyourtravel.Data
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jackpan.specialstudy.oveyouforyourtravel.R
+
+class FavoriteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_list)
+    }
+}

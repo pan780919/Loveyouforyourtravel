0d38cfe69e5a00a2377833b29f445d9eca228af1
pan780919 <jheng.wei0919@gmail.com>
2018-06-25 14:53:03 +0800

    add Adapter  and add listview

--- app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/Data/FavoriteListActivity.kt
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/Data/FavoriteListActivity.kt
@@ -2,9 +2,15 @@ package com.jackpan.specialstudy.oveyouforyourtravel.Data
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.hendraanggrian.pikasso.picasso
 import com.jackpan.libs.mfirebaselib.MfiebaselibsClass
 import com.jackpan.libs.mfirebaselib.MfirebaeCallback
 import com.jackpan.specialstudy.oveyouforyourtravel.R
+import java.util.ArrayList
 
 class FavoriteListActivity : AppCompatActivity(),MfirebaeCallback {
     override fun getUserLogoutState(p0: Boolean) {
@@ -48,9 +54,53 @@ class FavoriteListActivity : AppCompatActivity(),MfirebaeCallback {
     }
 
     lateinit var mFirebselibClass : MfiebaselibsClass
+    lateinit var mFavoriteList : ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mFirebselibClass = MfiebaselibsClass(this,this)
         setContentView(R.layout.activity_favorite_list)
+        mFavoriteList = findViewById(R.id.favoritelist)
+    }
+
+    inner class MyAdapter(var mAllData: ArrayList<GoogleResponseData.Results>?) : BaseAdapter() {
+        fun updateData(datas: ArrayList<GoogleResponseData.Results>) {
+            mAllData = datas
+            notifyDataSetChanged()
+        }
+
+        override fun getCount(): Int {
+            return mAllData!!.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return mAllData!![position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var convertView = convertView
+            val data = mAllData!![position]
+            if (convertView == null)
+                convertView = LayoutInflater.from(this@FavoriteListActivity).inflate(
+                        R.layout.listview_layout, null)
+            var mTittleText :TextView = convertView!!.findViewById(R.id.listviewtext)
+            var photoimg :ImageView = convertView!!.findViewById(R.id.listview_img)
+            mTittleText.text =data.name
+            if(data.photos!=null){
+//                var  photoString:String = GoogleMapAPISerive.getPhotos(this@TypeListViewActivity,data.photos.get(0).photo_reference)
+                picasso.load("").into(photoimg)
+            }else{
+                picasso.load(R.mipmap.nolodingphoto).into(photoimg)
+
+            }
+
+
+
+            return convertView
+        }
+
     }
 }

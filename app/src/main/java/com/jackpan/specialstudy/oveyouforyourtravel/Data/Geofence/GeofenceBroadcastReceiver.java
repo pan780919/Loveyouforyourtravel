ccfd1710ad3149fd34fe7605b6a05b3b5076b242
pan780919 <jheng.wei0919@gmail.com>
2018-06-29 12:52:25 +0800

    add geofence

---
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/Data/Geofence/GeofenceBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package com.jackpan.specialstudy.oveyouforyourtravel.Data.Geofence;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class GeofenceBroadcastReceiver  extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GeofenceTransitionsJobIntentService.enqueueWork(context, intent);
+
+    }
+}

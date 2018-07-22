99413059364bc51ac889ed5cc2b602c6fd8cd784
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-07-23 00:24:52 +0800

    拿掉 首頁 alertdilog （已登入）

--- app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/HomePageActivity.kt
+++ app/src/main/java/com/jackpan/specialstudy/oveyouforyourtravel/HomePageActivity.kt
@@ -178,11 +178,11 @@ class HomePageActivity : AppCompatActivity(), MfirebaeCallback{
             mAlertDilog.show()
             return false
         }else{
-            var mAlertDilog =AlertDialog.Builder(this)
-            mAlertDilog.setTitle("已登入")
-            mAlertDilog.setMessage("您已經可以使用此功能")
-            mAlertDilog.setPositiveButton("知道了！",null)
-            mAlertDilog.show()
+//            var mAlertDilog =AlertDialog.Builder(this)
+//            mAlertDilog.setTitle("已登入")
+//            mAlertDilog.setMessage("您已經可以使用此功能")
+//            mAlertDilog.setPositiveButton("知道了！",null)
+//            mAlertDilog.show()
             return true
         }
 
